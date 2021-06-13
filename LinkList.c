dab140d3b5b50021315cdae49415867748598d10
2205794866 <2205794866@qq.com>
2021-06-13 08:03:05 +0800

    init

--- LinkList.c
+++ LinkList.c
@@ -12,8 +12,26 @@ typedef struct _Node
     struct _Node *next;
 } Node;
 
-typedef struct _List
+typedef struct _LinkList
 {
     Node *head;
     Node *tail;
-} List;
+} LinkList;
+
+//定义状态Status
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+typedef int Status;
+
+//单链表的读取
+//用e返回L中第i个数据元素的值
+Status GetElem(LinkList *L, int i, ElemType *e)
+{
+    int j;
+    Node *p;
+    
+
+
+}
